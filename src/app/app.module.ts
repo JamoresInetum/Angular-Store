f626a44c150736350aacb832d0f44324b598bd74
JamoresInetum <joel.amores.ext@inetum.com>
2023-02-07 12:47:09 +0100

    rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { JoelComponent } from './pages/joel/joel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    JoelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    AppRoutingModule,
+    FormsModule,
+    MaterialModule,
+    BrowserAnimationsModule
+  ], // Modulos de angular necesarios
+  providers: [], //servicios, dependencias
+  bootstrap: [AppComponent] // Nombre del módulo principal
 })
 export class AppModule { }
---
+++ src/app/pages/joel/joel.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector:'app-joel',
+    templateUrl: './joel.component.html',
+    styleUrls: ['./joel.component.scss']
+})
+
+export class JoelComponent implements OnInit{
+
+    constructor(){ }
+    ngOnInit():void { }
+}
