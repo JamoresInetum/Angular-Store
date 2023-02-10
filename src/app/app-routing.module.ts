4fb727c4e247b3a69123df19d021351183b75615
JamoresInetum <joel.amores.ext@inetum.com>
2023-02-10 08:58:24 +0100

    Modificar Header: Links y Redirecciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   // Siempre se ejecutan en orden, tienen prioridades, util por si hay mantenimiento, por ejemplo
   // {path: '**', redirectTo:'mantenimiento', pathMatch: 'full'},
-  
+
+  { path: '', redirectTo: '/products', pathMatch: 'full'},
   { path: 'products', loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule) },
   { path: 'checkout', loadChildren: () => import('./pages/checkout/checkout.module').then(m => m.CheckoutModule) },
   {path: '**', redirectTo: '', pathMatch: 'full'}
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -6,14 +7,23 @@ import { Component } from '@angular/core';
   template: `
   
   <mat-toolbar color="primary">
-    <span>My Store </span>
+    <a [routerLink]="['/']"]><span>My Store </span></a>
+    
     <span class="spacer"></span>
-    <app-cart></app-cart>
+    <!-- Al hacer click al módulo del carrito, me lleva a la pagina checkout ! ALERTA ! No tendrá el pointer -->
+    <app-cart class="mouseHover" (click)="irAlCheckout()"></app-cart>
   </mat-toolbar>
   `,
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
+  // Para usar el router.navigate() necesito inyectar el Router de Angular
+  constructor(private router:Router){ }
+
+  irAlCheckout() {
+    this.router.navigate(['/checkout']);
+  }
 
   
   
